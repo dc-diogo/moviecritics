b3069d1c475866d4f461041a940914f83ad81913
Diogo Viana <diogo.viana@evoluum.com.br>
2022-11-03 19:57:20 -0300

    Corrigindo uso do h2

--- src/main/java/com/example/moviecritics/gateway/repository/MovieRepository.java
+++ src/main/java/com/example/moviecritics/gateway/repository/MovieRepository.java
@@ -1,11 +1,12 @@
 package com.example.moviecritics.gateway.repository;
 
 import com.example.moviecritics.domain.Movie;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface MovieRepository extends CrudRepository<Movie, Long> {
+public interface MovieRepository extends JpaRepository<Movie, Long> {
 
 
 }
